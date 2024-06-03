c1f6d19608d7d2213fdbc44e193958e944e76230
NewGen2022 <amirgimari@gmail.com>
2024-06-03 09:30:31 +0300

    added database creation and passing only instance to all classes

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from main_window import moviesWindow
 from login_window import login
+from db_connection import DataBase
+
 
 def run_application():
     app = QApplication(sys.argv)
 
+    database = DataBase()
+
     # Run the login window
-    login_window = login.Login()
+    login_window = login.Login(database)
     login_successful = login_window.run_login_process()
 
     if login_successful:
-        
-        main_window = moviesWindow.MainWindow()
+        main_window = moviesWindow.MainWindow(app, database)
+        main_window.show()
 
-        sys.exit(app.exec_())
+    sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     run_application()
