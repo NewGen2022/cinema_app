a4b6cdf535bd2cd93a9e83fc29ba86170558ea00
NewGen2022 <amirgimari@gmail.com>
2024-06-27 12:18:43 +0300

    changed to pyside6 and adjusted setup_ui

--- main_window/movieCard.py
+++ main_window/movieCard.py
@@ -1,32 +1,25 @@
-from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtWidgets import QLabel, QGridLayout, QWidget
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QFont, QPixmap
-
+from PySide6.QtCore import Signal, Qt
+from PySide6.QtGui import QFont, QPixmap
+from PySide6.QtWidgets import QLabel, QGridLayout, QWidget
 
 class MovieCard(QWidget):
-    movieClicked = pyqtSignal(
-        str
-    )  # Signal emitted when the movie card is clicked, passing the movie title
+    movieClicked = Signal(str)
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.make_card()  # Set up the UI components
+        self.make_card()
 
     def make_card(self):
-        self.setMaximumSize(300, 500)
+        self.setMaximumSize(300, 570)
 
-        # Create layout
         layout = QGridLayout(self)
         layout.setAlignment(Qt.AlignCenter)
         layout.setSpacing(0)
 
-        # Create label for movie poster
         self.poster_label = QLabel(self)
         self.poster_label.setAlignment(Qt.AlignCenter)
         self.poster_label.setScaledContents(True)
 
-        # Create label for movie title
         self.title_label = QLabel(self)
         self.title_label.setFixedHeight(50)
         self.title_label.setAlignment(Qt.AlignCenter)
@@ -34,24 +27,22 @@ class MovieCard(QWidget):
         self.title_label.setFont(title_font)
         self.title_label.setWordWrap(True)
 
-        # Add widgets to layout with vertical spacers
         layout.addWidget(self.poster_label, 1, 0, 1, 2)
         layout.addWidget(self.title_label, 2, 0, 1, 2)
 
-        # Connect the mouse press event to the on_mouse_press method
         self.mousePressEvent = self.on_mouse_press
 
     def set_card(self, title, poster_path):
-        self.title_label.setText(title)  # Set the movie title
-        self.set_movie_poster(poster_path)  # Set the movie poster
+        self.title_label.setText(title)
+        self.set_movie_poster(poster_path)
 
     def set_movie_poster(self, image_data):
         pixmap = QPixmap()
-        pixmap.loadFromData(image_data)  # Load the image data into the pixmap
-        self.poster_label.setMaximumSize(233, 344)
-        self.poster_label.setPixmap(pixmap)  # Set the pixmap on the poster label
+        pixmap.loadFromData(image_data)
+        self.poster_label.setMaximumSize(300, 443)
+        self.poster_label.setMinimumSize(300, 443)
+        self.poster_label.setPixmap(pixmap)
 
     def on_mouse_press(self, event):
-        self.movieClicked.emit(
-            self.title_label.text()
-        )  # Emit the movieClicked signal with the movie title when clicked
+        # print(f"Movie card clicked: {self.title_label.text()}")
+        self.movieClicked.emit(self.title_label.text())
--- main_window/moviesWindow.py
+++ main_window/moviesWindow.py
@@ -1,36 +1,35 @@
-from PyQt5.QtWidgets import QMainWindow, QScrollArea, QWidget, QHBoxLayout, QApplication
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QWheelEvent
-from PyQt5.uic import loadUi
+from PySide6.QtCore import QRect, QSize, Qt
+from PySide6.QtGui import QFont, QPixmap, QWheelEvent
+from PySide6.QtWidgets import (
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QScrollArea,
+    QSizePolicy,
+    QSpacerItem,
+    QVBoxLayout,
+    QWidget,
+)
 from main_window.movieCard import MovieCard
-from functools import partial
 from hall.session import SessionWindow
 
 
 class MainWindow(QMainWindow):
     def __init__(self, app, database):
-        super(MainWindow, self).__init__()
+        super().__init__()
         self.app = app
         self.database = database
-
         self.session_window = None
-
-        loadUi("./main_window/moviesUI.ui", self)
-
-        self.setWindowTitle("MainWindow")
-
-        screen_height = QApplication.desktop().screenGeometry().height()
-        self.setMinimumHeight(screen_height - 450)
-        screen_width = QApplication.desktop().screenGeometry().width()
-        self.setMinimumWidth(screen_width - 1000)
-
-        self.show()
-
+        self.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.setup_ui()
         self.connect_all_events()
+
         connection = self.database.connection
         cursor = connection.cursor()
         query_movie_info = """SELECT DISTINCT Sessions.id_kino,  kino.name_kino, kino.image_kino FROM db_findtick.Sessions
-join db_findtick.kino on Sessions.id_kino = kino.id_kino;"""
+                              JOIN db_findtick.kino ON Sessions.id_kino = kino.id_kino;"""
         cursor.execute(query_movie_info)
         self.movie_info = cursor.fetchall()
         self.movie_ids = []
@@ -42,6 +41,109 @@ join db_findtick.kino on Sessions.id_kino = kino.id_kino;"""
             self.movie_posters.append(item[2])
 
         self.display_all_movies()
+
+    def setup_ui(self):
+        self.resize(1920, 1080)
+
+        font = QFont()
+        font.setFamilies(["Century Gothic"])
+        font.setPointSize(12)
+        font.setBold(True)
+        self.setFont(font)
+        self.central_widget = QWidget(self)
+        self.central_widget.setObjectName("centralwidget")
+        self.vertical_layout = QVBoxLayout(self.central_widget)
+        self.vertical_layout.setObjectName("verticalLayout")
+        self.verticalSpacer = QSpacerItem(
+            20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed
+        )
+
+        self.vertical_layout.addItem(self.verticalSpacer)
+
+        self.horizontal_layout = QHBoxLayout()
+        self.horizontal_layout.setObjectName("horizontalLayout")
+        self.horizontal_spacer_3 = QSpacerItem(
+            150, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
+        )
+
+        self.horizontal_layout.addItem(self.horizontal_spacer_3)
+
+        self.logo_label = QLabel(self.central_widget)
+        self.logo_label.setObjectName("logo_label")
+        self.logo_label.setMinimumSize(QSize(120, 70))
+        self.logo_label.setMaximumSize(QSize(120, 70))
+        self.logo_label.setPixmap(QPixmap("./main_window/fintick-logo.png"))
+        self.logo_label.setScaledContents(True)
+
+        self.horizontal_layout.addWidget(self.logo_label)
+
+        self.horizontal_spacer = QSpacerItem(
+            120, 20, QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Minimum
+        )
+
+        self.horizontal_layout.addItem(self.horizontal_spacer)
+
+        self.search = QLineEdit(self.central_widget)
+        self.search.setObjectName("search")
+        self.search.setMaximumSize(QSize(16777215, 40))
+        font1 = QFont()
+        font1.setFamilies(["Century Gothic"])
+        font1.setPointSize(12)
+        font1.setBold(False)
+        self.search.setFont(font1)
+
+        self.search.setStyleSheet(
+            "border-radius: 10px;\n"
+            "padding-left: 10px;\n"
+            "background-color: rgb(230, 230, 230);\n"
+            ""
+        )
+        self.horizontal_layout.addWidget(self.search)
+
+        self.horizontalSpacer_2 = QSpacerItem(
+            120, 20, QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Minimum
+        )
+
+        self.horizontal_layout.addItem(self.horizontalSpacer_2)
+
+        self.help_button = QPushButton(self.central_widget)
+        self.help_button.setObjectName("help_button")
+        self.help_button.setStyleSheet("background-color: rgb(230, 230, 230);")
+        self.help_button.setMinimumSize(QSize(90, 35))
+        self.help_button.setMaximumSize(QSize(100, 40))
+
+        self.horizontal_layout.addWidget(self.help_button)
+
+        self.horizontalSpacer_4 = QSpacerItem(
+            150, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
+        )
+
+        self.horizontal_layout.addItem(self.horizontalSpacer_4)
+
+        self.vertical_layout.addLayout(self.horizontal_layout)
+
+        self.verticalSpacer_2 = QSpacerItem(
+            20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum
+        )
+
+        self.vertical_layout.addItem(self.verticalSpacer_2)
+
+        self.scroll_area = QScrollArea(self.central_widget)
+        self.scroll_area.setObjectName("scroll_area")
+        self.scroll_area.setLineWidth(0)
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area_widget_contents = QWidget()
+        self.scroll_area_widget_contents.setObjectName("scrollAreaWidgetContents")
+        self.scroll_area_widget_contents.setGeometry(QRect(0, 0, 1171, 429))
+        self.scroll_area.setWidget(self.scroll_area_widget_contents)
+
+        self.vertical_layout.addWidget(self.scroll_area)
+
+        self.setCentralWidget(self.central_widget)
+
+        self.setWindowTitle("MainWindow")
+        self.search.setPlaceholderText("Пошук")
+        self.help_button.setText("? Help")
 
     def handle_search(self):
         print("Search")
@@ -56,23 +158,45 @@ join db_findtick.kino on Sessions.id_kino = kino.id_kino;"""
         ):
             movie_card = MovieCard()
             movie_card.set_card(title, poster)
-            movie_card.movieClicked.connect(partial(self.on_movie_clicked, movie_id))
+
+            movie_card.movieClicked.connect(
+                lambda _, mid=movie_id: self.on_movie_clicked(mid)
+            )
             scroll_layout.addWidget(movie_card)
 
         scroll_widget.setLayout(scroll_layout)
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll_area.horizontalScrollBar().setStyleSheet(
+            """
+            QScrollArea {
+                border: none;
+            }  
+            QScrollBar:horizontal {
+                height: 7px;
+                border: none;
+            }
+            QScrollBar::handle:horizontal {
+                background-color: #2622fb;
+                border: none;
+            }
+            QScrollBar::add-page:horizontal {  
+                background-color: rgb(255, 255, 255);
+            }
+            QScrollBar::sub-page:horizontal {  
+                background-color: rgb(255, 255, 255); 
+            }
+            """
+        )
 
-        scroll_area.setWidgetResizable(True)
-        scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
-
-        scroll_area.setWidget(scroll_widget)
+        self.scroll_area.setWidget(scroll_widget)
 
     def connect_all_events(self):
         self.search.returnPressed.connect(self.handle_search)
 
     def on_movie_clicked(self, movie_id):
-        print("Movie clicked, ID:", movie_id)
-
         self.session_window = SessionWindow(self.app, movie_id, self.database)
         self.session_window.show()
 
